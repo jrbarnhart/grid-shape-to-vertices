12dd8314a15098ed08f2d3d4b50625d2ead38cb0
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-14 14:44:54 -0400

    Add base for GH pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/grid-shape-to-vertices",
   plugins: [react(), tailwindcss()],
 });
